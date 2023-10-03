b8f33b2b16df3d139d939daa8c620f6fb1862d3e
lucvnvc <32636740+lucvnvc@users.noreply.github.com>
2023-10-03 10:36:07 +0700

    Update firstTest.spec.ts

--- cypress/e2e/firstTest.spec.ts
+++ cypress/e2e/firstTest.spec.ts
@@ -1,7 +1,14 @@
 describe('My first test', () => {
-  it('launch browser and search', () => {
+  beforeEach(() => {
     cy.visit('https://www.google.com');
+  });
+  it('launch browser and search', () => {
     cy.get('[name="q"]').type('how to learn Cypress{enter}');
     cy.title().should('contain', 'how to learn Cypress');
+  });
+
+  it('attach screenshots when running tests fail', () => {
+    cy.get('[name="q"]').type('Cypress with TypeScript{enter}');
+    cy.url().should('contain', 'Cypress with TypeScript');
   });
 });
